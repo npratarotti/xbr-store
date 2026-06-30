005f61f9ac694ce76cd8f0cdc987aeb589b6ff14
npratarotti <natali.cardozo07@gmail.com>
2026-06-30 13:50:28 -0300

    feat(frontend): setup React application with Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-violet-500">
+          XBR Store
+        </h1>
+
+        <p className="mt-4 text-slate-400">
+          Nosso projeto Full Stack começou! 🚀
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default App
