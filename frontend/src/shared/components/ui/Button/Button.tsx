2ce2c0486a3feb01f7e69c55ee661ea9976e0c44
npratarotti <natali.cardozo07@gmail.com>
2026-07-01 12:19:11 -0300

    refactor(button): improve typing and fix build

--- frontend/src/shared/components/ui/Button/Button.tsx
+++ frontend/src/shared/components/ui/Button/Button.tsx
@@ -1,26 +1,26 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
 
 type ButtonProps = {
-    children: React.ReactNode
-    onClick?: () => void
-  }
-  
-  export function Button({ children, onClick }: ButtonProps) {
-    return (
-      <button
-        onClick={onClick}
-        className="
-          rounded-xl
-          bg-violet-600
-          px-6
-          py-3
-          font-semibold
-          text-white
-          transition
-          hover:bg-violet-700
-        "
-      >
-        {children}
-      </button>
-    )
-  }
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+export function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        rounded-xl
+        bg-violet-600
+        px-6
+        py-3
+        font-semibold
+        text-white
+        transition
+        hover:bg-violet-700
+      "
+    >
+      {children}
+    </button>
+  );
+}
